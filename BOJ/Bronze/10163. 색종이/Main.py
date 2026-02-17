848aa63b269e5f61300194a952bf5a98cca5171b
daesan12 <diqkraud@naver.com>
2026-02-17 21:14:28 +0900

    [Bronze I] Title: 색종이, Time: 296 ms, Memory: 39196 KB, Score: 53 point - DashHub

--- BOJ/Bronze/10163. 색종이/Main.py
+++ BOJ/Bronze/10163. 색종이/Main.py
@@ -2,6 +2,7 @@ N = int(input())
 xyarr = [list(map(int, input().split())) for _ in range(N)]
 arr = [[0]*1001 for _ in range(1001)]
 n = 1
+count = [0] * (N+1)
 for i in xyarr:
     y = i[0]
     x = i[1]
@@ -9,13 +10,15 @@ for i in xyarr:
     x_plus = i[3]
     for j in range(y,y+y_plus):
         for k in range(x,x+x_plus):
+            if arr[j][k] != 0:
+                count[arr[j][k]] -= 1
             arr[j][k] = n
+            count[arr[j][k]] += 1
     n += 1
 
+#for j in range(1001):
+#    for k in range(1001):
+#       count[arr[j][k]] += 1
+
 for i in range(1,N+1):
-    count = 0
-    for j in range(1001):
-        for k in range(1001):
-            if arr[j][k] == i:
-                count += 1
-    print(count)
+    print(count[i])
