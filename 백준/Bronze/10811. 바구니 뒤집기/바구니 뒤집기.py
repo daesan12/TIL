5ba3fe8d76f64cf8b87c8d82d8d6ebe09292042d
daesan12 <diqkraud@naver.com>
2026-03-29 23:52:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+
+baskets = [i for i in range(1, N + 1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    baskets[i-1:j] = baskets[i-1:j][::-1]
+
+print(*baskets)
