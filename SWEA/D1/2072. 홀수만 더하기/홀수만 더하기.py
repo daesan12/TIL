a3d453f01a210bcac1f7c5326918f2086d96c0f1
daesan12 <diqkraud@naver.com>
2026-05-12 23:10:58 +0900

    [D1] Title: 홀수만 더하기, Time: 58 ms, Memory: 52,480 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    nums = list(map(int, input().split()))
+    total = 0
+
+    for num in nums:
+        if num % 2 == 1:
+            total += num
+
+    print(f'#{tc} {total}')
